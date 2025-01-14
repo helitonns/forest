53057af87d615cedba3a0c2d71ce396540526640
helitonns <helitonns@gmail.com>
2025-01-14 16:17:39 -0400

    Animacao entrada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from "tailwindcss/plugin";
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -10,6 +13,20 @@ module.exports = {
       },
     },
     extend: {
+      keyframes: {
+        slideIn: {
+          "0%": { opacity: 0, transform: "translateX(-20px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        ["slied-in"]: "slideIn 0.4s ease-in-out forwards",
+        ["fade-in"]: "fadeIn 0.4s ease-in-out forwards",
+      },
       colors: {
         verde: {
           200: "#ACEF75",
@@ -23,6 +40,20 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({addUtilities}) => {
+      function animationDelay(){
+        const delays = {};
+
+        for(let i = 0; i <= 12; i++){
+          delays[`.animate-${i}`] = {
+            "animation-delay": `${i * 100}ms`
+          }
+        }
+        return delays;
+      }
+      addUtilities(animationDelay());
+  })
+],
 };
 
